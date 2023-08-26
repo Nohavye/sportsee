b0071ae9346c696491459857c01692103b263664
Nohavye <noha.poncelet@gmail.com>
2023-08-26 17:24:32 +0200

    Initialisation du projet.

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+// React
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// Global Styles
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<React.StrictMode></React.StrictMode>)
