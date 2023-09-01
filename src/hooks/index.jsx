18971186626895a6450276c2062cc08ec7d181d6
Nohavye <noha.poncelet@gmail.com>
2023-09-01 08:57:52 +0200

    Hook personnalsé 'useFetch' pour la récupération des données via une API.

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -4,6 +4,55 @@ import { useCallback, useEffect, useState } from 'react'
 // Data Tools
 import { EntityFactory } from '../data/EntityFactory'
 
+/**
+ * Hook personnalisé pour effectuer des requêtes de données.
+ *
+ * @param {string} url - L'URL à partir de laquelle les données doivent être récupérées.
+ * @returns {Object} Les fonctions et les états pour gérer les requêtes de données.
+ */
+export function useFetch(url, route, key) {
+    const [data, setData] = useState({})
+    const [isLoading, setLoading] = useState(true)
+    const [isReloading, setReloading] = useState(false)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        if (!url) return
+        if (!route) return
+
+        const urlRoute = url + route
+
+        /**
+         * Effectue la requête de données.
+         */
+        async function fetchData() {
+            setLoading(true)
+
+            try {
+                const response = await fetch(urlRoute)
+                const data = await response.json()
+                key ? setData(data[key]) : setData(data)
+            } catch (error) {
+                console.error(error)
+                console.log('Erreuuuuur !!!')
+                setError(true)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchData()
+        setReloading(false)
+    }, [url, route, key, isReloading])
+
+    /**
+     * Recharge les données.
+     */
+    const reload = () => setReloading(true)
+
+    return { reload, data, isLoading, error }
+}
+
 export function useData(json_data, entityFormat) {
     const [data, setData] = useState({})
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,15 @@
 import { Container } from './styled'
+import { useFetch } from '../../hooks'
 
 function Page() {
+    const { reload, data, isLoading, error } = useFetch(
+        'http://localhost:3000',
+        '/user/18',
+        'data'
+    )
+
+    console.log(data)
+
     return (
         <Container>
             <h1>Page d'accueil</h1>
